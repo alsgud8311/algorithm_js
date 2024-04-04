62e095734576c19557157ec0b0e27d96d3d65453
Minhyung Cho <dup33333@hufs.ac.kr>
2024-04-04 21:47:42 +0900

    reverse & fib implement via recursion

---
+++ udemy_lecture/fib.js
@@ -0,0 +1,12 @@
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
+
+function fib(n) {
+  if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
